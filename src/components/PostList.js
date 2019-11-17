17611424e3015483e671c6cf9be5e9d32d3296d5
dorota <dorota.zelga@gmail.com>
2019-11-17 11:10:42 +0100

    PostList > action fetchPosts connected

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from "../actions";
 
 class PostList extends React.Component {
     render() {
@@ -6,4 +8,8 @@ class PostList extends React.Component {
     }
 }
 
-export default PostList;
+export default connect(
+    null,
+    {fetchPosts}
+)(PostList);
+// null, as there is not data to pass to this component. there should be only mapStateToProps statement
