41527e9943d5191a628e7b7044d4c5b5b0449466
dorota <dorota.zelga@gmail.com>
2019-11-17 12:10:05 +0100

    react-thunk used to fetch data from real api

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,10 @@ import {connect} from 'react-redux';
 import {fetchPosts} from "../actions";
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return <h1>Post List</h1>
     }
